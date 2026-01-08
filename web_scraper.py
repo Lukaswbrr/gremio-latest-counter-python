0b6151d75f7d43fb6e8a18120590c6ba5c92cf01
Lukaswbrr <49600807+Lukaswbrr@users.noreply.github.com>
2026-01-08 15:39:48 -0300

    Add web scraping functionality to retrieve latest matches for Gremio
    - Implemented a function to fetch the latest match data from Fotmob using requests and BeautifulSoup.
    - Utilized fake_useragent to randomize the User-Agent header for requests.
    - Parsed the JSON data embedded in the webpage and saved it to a file.
    - Extracted and printed recent match information in a formatted manner.

--- web_scraper.py
+++ web_scraper.py
@@ -1,3 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
+from fake_useragent import UserAgent
+import json
 
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+
+def get_latest_matches():
+    html = ""
+
+    try:
+        html = requests.get("https://www.fotmob.com/en/teams/9769/overview/gremio", headers=headers)
+        html.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP Error Occured: {err}")
+        return
+    
+    #with open("test_html.html", "w") as file:
+    #    file.write(html.text)
+
+    soup = BeautifulSoup(html.text, "lxml")
+
+    #with open("test_html.html", "w") as file:
+    #    file.write(soup.prettify())
+
+    json_data_element = soup.find("script", id="__NEXT_DATA__")
+
+    json_data = json.loads(json_data_element.text)
+    
+    json_formatted = json.dumps(json_data, indent=4)
+
+    with open("test_json.json", "w") as file:
+        file.write(json_formatted)
+
+    #json_table = json_data["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["table"][0]["data"]
+    
+    # gets recent matches
+    recent_matches = json_data["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["teamForm"]
+    
+    print(json.dumps(recent_matches, indent=4))
+
+
+get_latest_matches()
