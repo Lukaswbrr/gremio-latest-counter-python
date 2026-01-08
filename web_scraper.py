b8f0a986f0453a232e434818a2e82b8ddb0c178c
Lukaswbrr <49600807+Lukaswbrr@users.noreply.github.com>
2026-01-08 16:17:18 -0300

    Refactor web scraper functions for clarity and consistency

--- web_scraper.py
+++ web_scraper.py
@@ -6,7 +6,7 @@ import json
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
-def get_latest_matches():
+def get_gremio_data():
     html = ""
 
     try:
@@ -15,30 +15,31 @@ def get_latest_matches():
     except requests.exceptions.HTTPError as err:
         print(f"HTTP Error Occured: {err}")
         return
-    
-    #with open("test_html.html", "w") as file:
-    #    file.write(html.text)
 
     soup = BeautifulSoup(html.text, "lxml")
 
-    #with open("test_html.html", "w") as file:
-    #    file.write(soup.prettify())
-
     json_data_element = soup.find("script", id="__NEXT_DATA__")
-
     json_data = json.loads(json_data_element.text)
-    
-    json_formatted = json.dumps(json_data, indent=4)
-
-    with open("test_json.json", "w") as file:
-        file.write(json_formatted)
-
+    #json_formatted = json.dumps(json_data, indent=4)
     #json_table = json_data["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["table"][0]["data"]
-    
+
+    return json_data
+
+def get_latest_matches():
     # gets recent matches
-    recent_matches = json_data["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["teamForm"]
+    recent_matches = get_gremio_data()["props"]["pageProps"]["fallback"]["team-9769"]["overview"]["teamForm"]
     
-    print(json.dumps(recent_matches, indent=4))
+    return recent_matches
+
+def format_match_results(recent_matches):
+    for k in recent_matches:
+        match k["resultString"]:
+            case "W":
+                print(f"venceu! ({k['tooltipText']['homeTeam']} {k['tooltipText']['homeScore']} x {k['tooltipText']['awayScore']} {k['tooltipText']['awayTeam']})")
+            case "L":
+                print(f"perdeu! ({k['tooltipText']['homeTeam']} {k['tooltipText']['homeScore']} x {k['tooltipText']['awayScore']} {k['tooltipText']['awayTeam']})")
+        
 
 
-get_latest_matches()
+#print(get_latest_matches())
+format_match_results(get_latest_matches())
